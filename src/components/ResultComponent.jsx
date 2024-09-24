e315472fcf0fd7f346bde37b7dc616a8635d7259
Anmol Agrawal <88332977+anmode@users.noreply.github.com>
2024-09-24 16:51:05 +0530

    Update ResultComponent.jsx
    mall issues!

--- src/components/ResultComponent.jsx
+++ src/components/ResultComponent.jsx
@@ -1,40 +1,53 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
-import "../css/ResultComponent.css"
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../css/ResultComponent.css';
 
-export default function ResultComponent(){
-
-  // added new
+export default function ResultComponent() {
   const location = useLocation();
-  console.log(location)
+  const {
+    score,
+    totalQuestions,
+    answeredQuestions,
+    correctAnswers,
+    wrongAnswers,
+  } = location.state;
 
-  return(
+  const passingScore = totalQuestions * 0.7;
+  const message =
+    score >= passingScore
+      ? 'Congratulations! You passed the quiz.'
+      : 'You need more practice!';
+
+  return (
     <div className="result">
       <h1>Result</h1>
       <div className="score-section">
-        <h3>You need more practice!</h3>
-        <h1 className="score">Your score is {location.state.score}</h1>
+        <h3>{message}</h3>
+        <h1 className="score">Your score is {score}</h1>
         <div className="flex">
           <div className="details">
-            <h5>Totol number of quesions</h5>
+            <h5>Total number of questions</h5>
             <h5>Number of attempted questions</h5>
             <h5>Number of correct answers</h5>
             <h5>Number of wrong answers</h5>
           </div>
           <div className="number">
-            <h5>{location.state.totalQuestions}</h5>
-            <h5>{location.state.answeredQuestions}</h5>
-            <h5>{location.state.correctAnswer}</h5>
-            <h5>{location.state.wrongAnswer}</h5>
+            <h5>{totalQuestions}</h5>
+            <h5>{answeredQuestions}</h5>
+            <h5>{correctAnswers}</h5>
+            <h5>{wrongAnswers}</h5>
           </div>
         </div>
       </div>
-
       <div className="buttons">
-        <Link to="/play-quiz"><button className="play">Play Again</button></Link>
-        <Link to="/"><button className="home-result">Back to home</button></Link>
+        <Link to="/play-quiz">
+          <button className="play">Play Again</button>
+        </Link>
+        <Link to="/">
+          <button className="home-result">Back to home</button>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
